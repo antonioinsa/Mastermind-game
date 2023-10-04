21c8ceb7d437ce44751e11664fedbf0198c06809
Toni <antonioinsa@tutanota.com>
2023-10-04 17:23:04 -0500

    build: under construction play page

--- js/play.js
+++ js/play.js
@@ -13,34 +13,28 @@ box4.style.backgroundColor = savedColor4;
 
 
 // Función para obtener una clave de localStorage y almacenarla en un array
-const localStorageData = (color, localStorageArray) => {
+const localStorageDat = (color, localStorageArray) => {
     const colors = localStorage.getItem(color);
     if (colors !== null) {
         localStorageArray.push(colors);
         //localStorage.removeItem(color); // Opcional: eliminar la clave de localStorage si se desea
     }
 };
-
-// Crear un array para almacenar las claves
 const colorsArray = [];
-// Llamar a la función para obtener y almacenar una clave en el array
-localStorageData('color1', colorsArray);
-localStorageData('color2', colorsArray);
-localStorageData('color3', colorsArray);
-localStorageData('color4', colorsArray);
-
-// Función para mezclar aleatoriamente un array
-const arrayMixer = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    console.log("Array Mezclado:", array);
-    return array;
-    
+localStorageDat('color1', colorsArray);
+localStorageDat('color2', colorsArray);
+localStorageDat('color3', colorsArray);
+localStorageDat('color4', colorsArray);
+const randomArray = [];
+const numeroElementosAleatorios = 4;
+for (let i = 0; i < numeroElementosAleatorios; i++) {
+  const indiceAleatorio = Math.floor(Math.random() * colorsArray.length);
+  const elementoAleatorio = colorsArray[indiceAleatorio];
+  randomArray.push(elementoAleatorio);
 }
-console.log("Array Original:", colorsArray);
-
+// Imprimir el nuevo array con elementos aleatorios
+console.log(randomArray);
+console.log(colorsArray);
 
 // Agregar un evento clic al div de selección
 document.getElementById('box1').addEventListener('click', function () {
@@ -72,39 +66,54 @@ const rgbToHex = (rgb) => {
     return `#${r}${g}${b}`;
 };
 //Obtener los cuatro divs diferentes por sus IDs
-const color1 = document.getElementById('color1');
-const color2 = document.getElementById('color2');
-const color3 = document.getElementById('color3');
-const color4 = document.getElementById("color4");
+const color1 = document.getElementById('color1-1');
+const color2 = document.getElementById('color2-1');
+const color3 = document.getElementById('color3-1');
+const color4 = document.getElementById('color4-1');
 
+const color1 = document.getElementById('color1-2');
+const color2 = document.getElementById('color2-2');
+const color3 = document.getElementById('color3-2');
+const color4 = document.getElementById('color4-2');
+
+//const color1 = document.getElementById('color1-3');
+//const color2 = document.getElementById('color2-3');
+//const color3 = document.getElementById('color3-3');
+//const color4 = document.getElementById('color4-3');
+//
+//const color1 = document.getElementById('color1-4');
+//const color2 = document.getElementById('color2-4');
+//const color3 = document.getElementById('color3-4');
+//const color4 = document.getElementById('color4-4');
+//
+//const color1 = document.getElementById('color1-5');
+//const color2 = document.getElementById('color2-5');
+//const color3 = document.getElementById('color3-5');
+//const color4 = document.getElementById('color4-5');
+//
+//const color1 = document.getElementById('color1-6');
+//const color2 = document.getElementById('color2-6');
+//const color3 = document.getElementById('color3-6');
+//const color4 = document.getElementById('color4-6');
+//
+//const color1 = document.getElementById('color1-7');
+//const color2 = document.getElementById('color2-7');
+//const color3 = document.getElementById('color3-7');
+//const color4 = document.getElementById('color4-7');
 // Agregar un evento clic a cada uno de los divs
-color1.addEventListener('click', () => {
-    const choiceColor1 = localStorage.getItem('choiceColor');
-    this.style.backgroundColor = choiceColor1;
+color1.addEventListener('click', function () {
+    const choiceColor = localStorage.getItem('choiceColor');
+    this.style.backgroundColor = choiceColor;
 });
-color2.addEventListener('click', () => {
-    const choiceColor2 = localStorage.getItem('choiceColor');
-    this.style.backgroundColor = choiceColor2;
+color2.addEventListener('click', function () {
+    const choiceColor = localStorage.getItem('choiceColor');
+    this.style.backgroundColor = choiceColor;
 });
-color3.addEventListener('click', () => {
-    const choiceColor3 = localStorage.getItem('choiceColor');
-    this.style.backgroundColor = choiceColor3;
+color3.addEventListener('click', function () {
+    const choiceColor = localStorage.getItem('choiceColor');
+    this.style.backgroundColor = choiceColor;
 });
-color4.addEventListener("click", () => {
-    const choiceColor4 = localStorage.getItem("choiceColor");
-    this.style.backgroundColor = choiceColor4;
+color4.addEventListener("click", function () {
+    const choiceColor = localStorage.getItem('choiceColor');
+    this.style.backgroundColor = choiceColor;
 });
-// Función para obtener una clave de localStorage y almacenarla en un array
-const localStorageDat = (choiceColor, choiceArray) => {
-    const choiceColors = localStorage.getItem(choiceColor);
-    if (choiceColors !== null) {
-        choiceArray.push(choiceColor);
-        //localStorage.removeItem(color); // Opcional: eliminar la clave de localStorage si se desea
-    }
-};
-
-// Crear un array para almacenar las claves
-const choiceArray = [];
-// Llamar a la función para obtener y almacenar una clave en el array
-localStorageDat('choiceColor', choiceArray);
-console.log(choiceArray);
