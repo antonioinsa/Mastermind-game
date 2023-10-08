b6af105abc107bada7623d68b621b64be5b933ba
Toni <antonioinsa@tutanota.com>
2023-10-08 18:29:58 +0200

    feat:added restart game button for go back to index.html and clear  all localStorage key

---
+++ js/winner.js
@@ -0,0 +1,8 @@
+const restartGame = document.getElementById('restartGame');
+restartGame.addEventListener('click', () => {
+    const keys = Object.keys(localStorage);
+        keys.forEach(key => {
+        localStorage.removeItem(key);
+    });
+    window.location.href = '../index.html'
+});
